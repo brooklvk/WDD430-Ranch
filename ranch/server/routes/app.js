d4307e773e257cd70f1488c53f4bf180a893c197
Brooklyn Sowards <brooklynvkelsey@gmail.com>
2024-12-16 14:28:41 -0700

    app loads with server.js but no content

--- ranch/server/routes/app.js
+++ ranch/server/routes/app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
+  res.sendFile(path.join(__dirname, '../../src/index.html'));
 });
 
 module.exports = router;
