08ad3bb2ec7d1d0242cd05d35cbb06d1bf474b3e
Brooklyn Sowards <brooklynvkelsey@gmail.com>
2024-12-16 15:01:42 -0700

    Revert "app loads with server.js but no content"
    This reverts commit d4307e773e257cd70f1488c53f4bf180a893c197.

--- ranch/server/routes/app.js
+++ ranch/server/routes/app.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const path = require('path');
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../../src/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/cms/index.html'));
 });
 
 module.exports = router;
